da29c416e8db9c172c146734dfe8262a3e74ff31
Erin L. Kolp <erinlkolpfoss@gmail.com>
2025-02-26 18:13:21 -0700

    Saving progress.

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import logging
 import time
 import random
 
+# logging.basicConfig(
+#     format='%(asctime)s %(levelname)-8s %(message)s',
+#     level=logging.INFO,
+#     datefmt='%Y-%m-%d %H:%M:%S')
+
+# LOGGER = logging.getLogger(__name__)
+
+
 class VUMeter:
     def __init__(self, server_address: str, server_port: int, api_key: str):
         """
@@ -19,29 +27,83 @@ class VUMeter:
         self.server_url = f'http://{server_address}:{server_port}'
         self.key        = api_key
 
+    def list_dials(self) -> str:
+        """
+        This function list the connected vu-dials.
+
+        :param uuid: str, the uuid of the vu-dial.
+        :param value: int, value of dial position between 0 and 100.
+        :return result: str, returns the request query result.
+        """
+        api_uri = '/api/v0/dial/list'
+
+        try:
+            r = requests.get(f'{self.server_url}/{api_uri}?key={self.key}')
+        except Exception as exc:
+            raise exc
+
+        return r.text
+    
+    def get_dial_info(self, uuid: str) -> str:
+        """
+        This function sets the dial position.
+
+        :param uuid: str, the uuid of the vu-dial.
+        :param value: int, value of dial position between 0 and 100.
+        :return result: str, returns the request query result.
+        """
+        api_uri = f'/api/v0/dial/{uuid}/status'
+
+        try:
+            r = requests.get(f'{self.server_url}/{api_uri}?key={self.key}')
+        except Exception as exc:
+            raise exc
+
+        return r.text
+
+    def get_image_crc(self, uuid: str) -> str:
+        """
+        This function sets the dial position.
+
+        :param uuid: str, the uuid of the vu-dial.
+        :param value: int, value of dial position between 0 and 100.
+        :return result: str, returns the request query result.
+        """
+        api_uri = f'/api/v0/dial/{uuid}/image/crc'
+
+        try:
+            r = requests.get(f'{self.server_url}/{api_uri}?key={self.key}')
+        except Exception as exc:
+            raise exc
+
+        return r.text
+    
     def set_dial_value(self, uuid: str, value: int) -> str:
         """
-        This function executes and commits a mysql insert function.
+        This function sets the dial position.
 
-        :param query: str, a sql query with proper syntax.
-        :return True: bool, returns True if the insert is completed.
+        :param uuid: str, the uuid of the vu-dial.
+        :param value: int, value of dial position between 0 and 100.
+        :return result: str, returns the request query result.
         """
         api_uri = f'/api/v0/dial/{uuid}/set'
 
         try:
             r = requests.get(f'{self.server_url}/{api_uri}?key={self.key}&value={value}')
         except Exception as exc:
-            LOGGER.error("Error Result: %s", exc)
-            return exc
+            raise exc
 
         return r.text
 
     def set_dial_color(self, uuid: str, red: int, green: int, blue: int) -> str:
         """
-        This function executes and commits a mysql insert function.
+        This function sets the dial color.
 
-        :param query: str, a sql query with proper syntax.
-        :return True: bool, returns True if the insert is completed.
+        :param uuid: str, the uuid of the vu-dial.
+        :param red: int, the brightness of the red led - from 0 to 100.
+        :param green: the brightness of the green led - from 0 to 100.
+        :param blue: the brightness of the blue led - from 0 to 100.
+        :return result: str, returns the request query result.
         """
 
         api_uri = f'/api/v0/dial/{uuid}/backlight'
@@ -49,27 +111,36 @@ class VUMeter:
         try:
             r = requests.get(f'{self.server_url}/{api_uri}?key={self.key}&red={red}&green={green}&blue={blue}')
         except Exception as exc:
-            LOGGER.error("Error Result: %s", exc)
-            return exc
+            raise exc
 
         return r.text
 
-if __name__ == "__main__":
-    while True:
-        dial_uuid    = os.environ['TARGET_DIAL_UID']
-        server_key     = os.environ['API_KEY']
-        srv_address = os.environ['VU1_SERVER_ADDRESS']
-        srv_port    = os.environ['VU1_SERVER_PORT']
+# if __name__ == "__main__":
+#     while True:
+#         dial_uuid    = os.environ['TARGET_DIAL_UID']
+#         server_key     = os.environ['API_KEY']
+#         srv_address = os.environ['VU1_SERVER_ADDRESS']
+#         srv_port    = os.environ['VU1_SERVER_PORT']
 
-        red = random.randint(0, 100)
-        green = random.randint(0, 100)
-        blue = random.randint(0, 100)
+#         red = random.randint(0, 100)
+#         green = random.randint(0, 100)
+#         blue = random.randint(0, 100)
 
-        value = random.randint(0, 100)
+#         value = random.randint(0, 100)
 
-        vu_meter  = VUMeter(srv_address, srv_port, server_key)
-        result = vu_meter.set_dial_value(dial_uuid, value)
-        time.sleep(1)
-        LOGGER.info("Response: %s", result)
-        result = vu_meter.set_dial_color(dial_uuid, red, green, blue)
-        time.sleep(1)
+#         vu_meter  = VUMeter(srv_address, srv_port, server_key)
+#         result = vu_meter.set_dial_value(dial_uuid, value)
+#         LOGGER.info("Response: %s", result)
+#         time.sleep(1)
+
+#         result = vu_meter.list_dials()
+#         LOGGER.info("Response: %s", result)
+#         time.sleep(1)
+
+#         result = vu_meter.get_dial_info(dial_uuid)
+#         LOGGER.info("Response: %s", result)
+#         time.sleep(1)
+                
+#         result = vu_meter.get_image_crc(dial_uuid)
+#         LOGGER.info("Response: %s", result)
+#         time.sleep(1)
