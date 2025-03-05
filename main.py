65160bd69a822e98117259ab78fab5958eaeb689
Erin L. Kolp <erinlkolpfoss@gmail.com>
2025-03-04 17:19:00 -0700

    Fixes file closure.

--- main.py
+++ main.py
@@ -113,8 +113,9 @@ class VUMeter:
         api_uri = f'/api/v0/dial/{uid}/image/set'
 
         try:
-            files = {'imgfile': open(f'{file}', 'rb')}
-            r = requests.post(f'{self.server_url}/{api_uri}?key={self.key}',files=files)
+            with open(f'{file}', 'rb') as file:
+                files = {'imgfile': file}
+                r = requests.post(f'{self.server_url}/{api_uri}?key={self.key}',files=files)
         except Exception as exc:
             raise exc
 
@@ -152,3 +153,22 @@ class VUMeter:
             raise exc
 
         return json.loads(r.text)
+
+if __name__ == "__main__":
+    dial_uid    = os.environ['TARGET_DIAL_UID']
+    server_key     = os.environ['API_KEY']
+    srv_address = os.environ['VU1_SERVER_ADDRESS']
+    srv_port    = os.environ['VU1_SERVER_PORT']
+
+    while True:
+        red = random.randint(0, 100)
+        green = random.randint(0, 100)
+        blue = random.randint(0, 100)
+
+        value = random.randint(0, 100)
+
+        vu_meter  = VUMeter(srv_address, srv_port, server_key)
+
+        result = vu_meter.set_dial_background(dial_uid, '/Users/ekolp/workspace/vu1-dial-python-module/sample.png')
+        print(result)
+        time.sleep(1)
