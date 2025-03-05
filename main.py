58d9d011e3d14e8f0923c153a7fc3e5304af0ace
Erin L. Kolp <erinlkolpfoss@gmail.com>
2025-03-04 17:20:04 -0700

    Comment out test lines.

--- main.py
+++ main.py
@@ -154,21 +154,21 @@ class VUMeter:
 
         return json.loads(r.text)
 
-if __name__ == "__main__":
-    dial_uid    = os.environ['TARGET_DIAL_UID']
-    server_key     = os.environ['API_KEY']
-    srv_address = os.environ['VU1_SERVER_ADDRESS']
-    srv_port    = os.environ['VU1_SERVER_PORT']
+# if __name__ == "__main__":
+#     dial_uid    = os.environ['TARGET_DIAL_UID']
+#     server_key     = os.environ['API_KEY']
+#     srv_address = os.environ['VU1_SERVER_ADDRESS']
+#     srv_port    = os.environ['VU1_SERVER_PORT']
 
-    while True:
-        red = random.randint(0, 100)
-        green = random.randint(0, 100)
-        blue = random.randint(0, 100)
+#     while True:
+#         red = random.randint(0, 100)
+#         green = random.randint(0, 100)
+#         blue = random.randint(0, 100)
 
-        value = random.randint(0, 100)
+#         value = random.randint(0, 100)
 
-        vu_meter  = VUMeter(srv_address, srv_port, server_key)
+#         vu_meter  = VUMeter(srv_address, srv_port, server_key)
 
-        result = vu_meter.set_dial_background(dial_uid, '/Users/ekolp/workspace/vu1-dial-python-module/sample.png')
-        print(result)
-        time.sleep(1)
+#         result = vu_meter.set_dial_background(dial_uid, '/Users/ekolp/workspace/vu1-dial-python-module/sample.png')
+#         print(result)
+#         time.sleep(1)
